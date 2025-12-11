9fd2c928545d0a03d198089efab05d7967683156
RaphBi <2raphael.bigey@gmail.com>
2025-12-11 16:47:54 +0100

    class_row_element.hpp terminé

---
+++ prog_2/DM/class_row_element.hpp
@@ -0,0 +1,175 @@
+#pragma once 
+#include "header/class_dynamic_vector.hpp"
+#include "header/class_linked_list.hpp"
+#include "header/class_list.hpp"
+
+
+template<typename T>
+class row_element{
+    protected: 
+        T value_;
+        int column_;
+
+    public:
+        // constructors
+        row_element(T value=0, int column=-1): value_(value), column_(column){}; 
+        row_element<T>& operator=(const row_element<T>& other);
+
+        // getters and setters
+        inline T get_value() const { return value_;}
+        inline int get_column() const { return column_;}
+        inline void set_value(T value){ value_ = value;}
+        inline void set_column(int column){ column_ = column;}
+
+        // basic operators
+        row_element<T>& operator+=(T value);
+        row_element<T>& operator+=(const row_element<T>& other);
+        row_element<T>& operator-=(T value);
+        row_element<T>& operator-=(const row_element<T>& other);
+        row_element<T>& operator*=(T value);
+        row_element<T>& operator*=(const row_element<T>& other);
+        row_element<T>& operator/=(T value);
+        row_element<T>& operator/=(const row_element<T>& other);
+
+};
+
+
+// copy constructor 
+template<typename T>
+row_element<T>& row_element<T>::operator=(const row_element<T>& other){
+    if(this != &other){
+        this->value_ = other.get_value();
+        this->column_ = other.get_column();
+    }
+    return *this;
+}
+
+
+
+// basic operators
+template<typename T>
+row_element<T>& row_element<T>::operator+=(T value){
+    this->set_value(this->get_value() + value);
+    return *this;
+}
+
+template<typename T>
+row_element<T>& row_element<T>::operator+=(const row_element<T>& other){
+    this->set_value(this->get_value() + other.get_value());
+    return *this;
+}
+
+template<typename T>
+row_element<T>& row_element<T>::operator-=(T value){
+    this->set_value(this->get_value() - value);
+    return *this;
+}
+
+template<typename T>
+row_element<T>& row_element<T>::operator-=(const row_element<T>& other){
+    this->set_value(this->get_value() - other.get_value());
+    return *this;
+}
+
+template<typename T>
+row_element<T>& row_element<T>::operator*=(T value){
+    this->set_value(this->get_value() * value);
+    return *this;
+}
+
+template<typename T>
+row_element<T>& row_element<T>::operator*=(const row_element<T>& other){
+    this->set_value(this->get_value() * other.get_value());
+    return *this;
+}
+
+template<typename T>
+row_element<T>& row_element<T>::operator/=(T value){
+    this->set_value(this->get_value() / value);
+    return *this;
+}
+
+template<typename T>
+row_element<T>& row_element<T>::operator/=(const row_element<T>& other){
+    this->set_value(this->get_value() / other.get_value());
+    return *this;
+}
+
+
+
+// column comparator
+template<typename T>
+int operator<(const row_element<T>& re1, const row_element<T>& re2){
+    return re1.get_column() < re2.get_column();
+}
+
+template<typename T>
+int operator>(const row_element<T>& re1, const row_element<T>& re2){
+    return re1.get_column() > re2.get_column();
+}
+
+template<typename T>
+int operator==(const row_element<T>& re1, const row_element<T>& re2){
+    return re1.get_column() == re2.get_column();
+}
+
+
+
+
+// arithmetic operators
+
+template<typename T>
+const row_element<T> operator+(row_element<T> const& e, T const& t){
+    row_element<T> res = e;
+    res += t;
+    return res;
+}
+
+template<typename T>
+const row_element<T> operator+(T const& t , row_element<T> const& e){
+    row_element<T> res = e;
+    res += t;
+    return res;
+}
+
+template<typename T>
+const row_element<T> operator-(row_element<T> const& e, T const& t){
+    row_element<T> res = e;
+    res -= t;
+    return res;
+}
+
+template<typename T>
+const row_element<T> operator-(T const& t , row_element<T> const& e){
+    row_element<T> res = e;
+    res.set_value(t - res.get_value());
+    return res;
+}
+
+template<typename T>
+const row_element<T> operator*(row_element<T> const& e, T const& t){
+    row_element<T> res = e;
+    res *= t;
+    return res;
+}
+
+template<typename T>
+const row_element<T> operator*(T const& t , row_element<T> const& e){
+    row_element<T> res = e;
+    res *= t;
+    return res;
+}
+
+
+template<typename T>
+const row_element<T> operator/(row_element<T> const& e, T const& t){
+    row_element<T> res = e;
+    res /= t;
+    return res;
+}
+
+
+template<typename T>
+void print(const row_element<T>& re){
+    std::cout << "(val: " << re.get_value() << ", col: " << re.get_column() << ") ";
+}
